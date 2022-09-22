e84d4554464bdf973346ba8749379d7482f9dd3e
Jefferson Soares <jefferson1104junior@gmail.com>
2022-09-22 11:59:53 -0300

    components DuoCard, DuoInfo, called api in Game Page

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,4 +1,5 @@
-import { Image, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -7,6 +8,7 @@ import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 
 import { GameParams } from '../../@types/navigation';
 
@@ -14,16 +16,22 @@ import { THEME } from '../../theme';
 import { styles } from './styles';
 
 
-
-
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
   const navigation = useNavigation();
   const route = useRoute();
+  
   const game = route.params as GameParams;
 
   function handleGoBack() {
     navigation.goBack()
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.1.210:3333/games/${game.id}/ads`)
+    .then(response => response.json())
+    .then(data => setDuos(data));
+  }, []);
 
   return (
     <Background>
@@ -53,6 +61,18 @@ export function Game() {
         <Heading 
           title={game.title}
           subtitle="Conecte-se e comece a jogar!"
+        />
+
+        <FlatList 
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={styles.contentList}
+          showsHorizontalScrollIndicator={false}
         />
       </SafeAreaView>
     </Background>
