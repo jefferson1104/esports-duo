57a6c32926cbb7dade1e1077293ced102e2c1755
Jefferson Soares <jefferson1104junior@gmail.com>
2022-09-23 11:30:00 -0300

    finish web

---
+++ web/README.md
@@ -0,0 +1,5 @@
+## Melhorias a fazer
+- Responsividade
+- Criar carrousel para listar mais que 6 jogos (utilizar keen-slider)
+- Fazer validacao de dados (utilizar react hook form com yup, zod, joi e etc...)
+- Colocar autenticacao com Discord, Twitch ou qualquer JWT.
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import * as Dialog from '@radix-ui/react-dialog';
 
 import { GameBanner } from './components/GameBanner';
@@ -22,11 +23,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([])
 
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data)
-      })
+    axios('http://localhost:3333/games').then(response => {
+      setGames(response.data)
+    })
   }, [])
 
   return (
